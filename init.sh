28cd7a636416d0ebf1f7b200667764b283c283b3
Jonathan Fok kan <jonathan.fokkan@appdirect.com>
2022-12-22 21:36:58 -0500

    Updated init.sh

--- init.sh
+++ init.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
-stow bash -t $HOME
 stow vim -t $HOME
-stow shell -t $HOME
-stow spacemacs -t $HOME
-stow tmux -t $HOME
+stow zsh -t $HOME
+stow git -t $HOME
+
 touch $HOME/.env_priv
