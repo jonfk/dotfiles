45499314386fab6d086e4b39a3b4f9fe0e6154e4
Jonathan Fok kan <jonathan@fokkan.ca>
2024-07-25 11:46:00 -0400

    nvim: Added gopls to lsp config

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -573,45 +573,43 @@ require("lazy").setup({
 			--  - capabilities (table): Override fields in capabilities. Can be used to disable certain LSP features.
 			--  - settings (table): Override the default settings passed when initializing the server.
 			--        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
-			local servers = {
-				-- clangd = {},
-				-- gopls = {},
-				-- pyright = {},
-				-- rust_analyzer = {},
-				-- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
-				--
-				-- Some languages (like typescript) have entire language plugins that can be useful:
-				--    https://github.com/pmizio/typescript-tools.nvim
-				--
-				-- But for many setups, the LSP (`tsserver`) will work just fine
-				-- tsserver = {},
-				--
+			local servers =
+				{
+					-- clangd = {},
+					gopls = {},
+					-- pyright = {},
+					-- rust_analyzer = {},
+					-- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
+					--
+					-- Some languages (like typescript) have entire language plugins that can be useful:
+					--    https://github.com/pmizio/typescript-tools.nvim
+					--
+					-- But for many setups, the LSP (`tsserver`) will work just fine
+					-- tsserver = {},
+					--
 
-				lua_ls = {
-					-- cmd = {...},
-					-- filetypes = { ...},
-					-- capabilities = {},
-					settings = {
-						Lua = {
-							completion = {
-								callSnippet = "Replace",
+					lua_ls = {
+						-- cmd = {...},
+						-- filetypes = { ...},
+						-- capabilities = {},
+						settings = {
+							Lua = {
+								completion = {
+									callSnippet = "Replace",
+								},
+								-- You can toggle below to ignore Lua_LS's noisy `missing-fields` warnings
+								-- diagnostics = { disable = { 'missing-fields' } },
 							},
-							-- You can toggle below to ignore Lua_LS's noisy `missing-fields` warnings
-							-- diagnostics = { disable = { 'missing-fields' } },
 						},
 					},
 				},
-			}
-
-			gopls = {}
-
-			-- Ensure the servers and tools above are installed
-			--  To check the current status of installed tools and/or manually install
-			--  other tools, you can run
-			--    :Mason
-			--
-			--  You can press `g?` for help in this menu.
-			require("mason").setup()
+				-- Ensure the servers and tools above are installed
+				--  To check the current status of installed tools and/or manually install
+				--  other tools, you can run
+				--    :Mason
+				--
+				--  You can press `g?` for help in this menu.
+				require("mason").setup()
 
 			-- You can add other tools here that you want Mason to install
 			-- for you, so that they are available from within Neovim.
@@ -903,9 +901,7 @@ require("lazy").setup({
 		"akinsho/bufferline.nvim",
 		version = "*",
 		dependencies = "nvim-tree/nvim-web-devicons",
-		config = function()
-			require("bufferline").setup({})
-		end,
+		opts = {},
 	},
 
 	-- The following two comments only work if you have downloaded the kickstart repo, not just copy pasted the
