5630e91f95575c5e7cc4909785b51566d01c6bee
Jonathan Fok kan <jonathan@fokkan.ca>
2024-08-12 12:25:51 -0400

    nvim: Fixed lua errors in config

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -475,49 +475,48 @@ require("lazy").setup({
 			--  - capabilities (table): Override fields in capabilities. Can be used to disable certain LSP features.
 			--  - settings (table): Override the default settings passed when initializing the server.
 			--        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
-			local servers =
-				{
-					-- clangd = {},
-					gopls = {},
-					-- pyright = {},
-					rust_analyzer = {},
-					-- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
-					--
-					-- Some languages (like typescript) have entire language plugins that can be useful:
-					--    https://github.com/pmizio/typescript-tools.nvim
-					--
-					-- But for many setups, the LSP (`tsserver`) will work just fine
-					-- tsserver = {},
+			local servers = {
+				-- clangd = {},
+				gopls = {},
+				-- pyright = {},
+				rust_analyzer = {},
+				-- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
+				--
+				-- Some languages (like typescript) have entire language plugins that can be useful:
+				--    https://github.com/pmizio/typescript-tools.nvim
+				--
+				-- But for many setups, the LSP (`tsserver`) will work just fine
+				-- tsserver = {},
 
-					jsonls = {
-						settings = {
-							json = {
-								validate = { enable = true },
-							},
-						},
-					},
-					lua_ls = {
-						-- cmd = {...},
-						-- filetypes = { ...},
-						-- capabilities = {},
-						settings = {
-							Lua = {
-								completion = {
-									callSnippet = "Replace",
-								},
-								-- You can toggle below to ignore Lua_LS's noisy `missing-fields` warnings
-								-- diagnostics = { disable = { 'missing-fields' } },
-							},
+				jsonls = {
+					settings = {
+						json = {
+							validate = { enable = true },
 						},
 					},
 				},
-				-- Ensure the servers and tools above are installed
-				--  To check the current status of installed tools and/or manually install
-				--  other tools, you can run
-				--    :Mason
-				--
-				--  You can press `g?` for help in this menu.
-				require("mason").setup()
+				lua_ls = {
+					-- cmd = {...},
+					-- filetypes = { ...},
+					-- capabilities = {},
+					settings = {
+						Lua = {
+							completion = {
+								callSnippet = "Replace",
+							},
+							-- You can toggle below to ignore Lua_LS's noisy `missing-fields` warnings
+							-- diagnostics = { disable = { 'missing-fields' } },
+						},
+					},
+				},
+			}
+			-- Ensure the servers and tools above are installed
+			--  To check the current status of installed tools and/or manually install
+			--  other tools, you can run
+			--    :Mason
+			--
+			--  You can press `g?` for help in this menu.
+			require("mason").setup()
 
 			-- You can add other tools here that you want Mason to install
 			-- for you, so that they are available from within Neovim.
@@ -842,9 +841,9 @@ require("lazy").setup({
 	{
 		"akinsho/toggleterm.nvim",
 		version = "*",
-		config = {
-			vim.keymap.set("n", "<leader>tt", "<cmd>ToggleTerm<cr>", { desc = "ToggleTerm Terminal" }),
-		},
+		config = function()
+			vim.keymap.set("n", "<leader>tt", "<cmd>ToggleTerm<cr>", { desc = "ToggleTerm Terminal" })
+		end,
 	},
 	{
 		"stevearc/oil.nvim",
@@ -852,9 +851,9 @@ require("lazy").setup({
 		-- Optional dependencies
 		dependencies = { { "echasnovski/mini.icons", opts = {} } },
 		-- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if prefer nvim-web-devicons
-		config = {
-			vim.keymap.set("n", "<leader>ee", "<cmd>Oil<cr>", { desc = "Open Oil" }),
-		},
+		config = function()
+			vim.keymap.set("n", "<leader>ee", "<cmd>Oil<cr>", { desc = "Open Oil" })
+		end,
 	},
 	{
 		"numToStr/Comment.nvim",
