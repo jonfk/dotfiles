16ced2afd159a627458e97f1a94d00cefb6f123b
Jonathan Fok kan <jfokkan@gmail.com>
2025-02-18 18:23:48 -0500

    nvim: Added flash

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -154,6 +154,7 @@ vim.opt.rtp:prepend(lazypath)
 --
 -- NOTE: Here is where you install your plugins.
 require("lazy").setup({
+	{ import = "plugins" },
 	-- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
 	"tpope/vim-sleuth", -- Detect tabstop and shiftwidth automatically
 
