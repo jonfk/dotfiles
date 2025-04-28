07ffb6a17643e58e76612f46ef4e46b04a3cd8d8
Jonathan Fok kan <jonathan@fokkan.ca>
2025-04-28 10:55:13 -0400

    feat(nvim): add clipboard paste keybindings for macOS

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -12,7 +12,18 @@ if vim.g.neovide then
 	vim.g.neovide_position_animation_length = 0.15
 	vim.g.neovide_scroll_animation_length = 0.05
 	vim.g.neovide_input_macos_option_key_is_meta = "only_left"
+
+	vim.keymap.set("n", "<D-v>", '"+P') -- Paste normal mode
+	vim.keymap.set("v", "<D-v>", '"+P') -- Paste visual mode
+	vim.keymap.set("c", "<D-v>", "<C-R>+") -- Paste command mode
+	vim.keymap.set("i", "<D-v>", '<ESC>l"+Pli') -- Paste insert mode
 end
+
+-- Allow clipboard copy paste in neovim
+vim.api.nvim_set_keymap("", "<D-v>", "+p<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("!", "<D-v>", "<C-R>+", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("t", "<D-v>", "<C-R>+", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("v", "<D-v>", "<C-R>+", { noremap = true, silent = true })
 
 -- [[ Setting options ]]
 -- See `:help vim.opt`
