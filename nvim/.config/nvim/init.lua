98211cf593a64dc968434abedb62661bba97671a
Jonathan Fok kan <jonathan@fokkan.ca>
2025-05-01 21:17:38 -0400

    nvim: Added clipboard keymaps desc for neovide

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -13,17 +13,41 @@ if vim.g.neovide then
 	vim.g.neovide_scroll_animation_length = 0.05
 	vim.g.neovide_input_macos_option_key_is_meta = "only_left"
 
-	vim.keymap.set("n", "<D-v>", '"+P') -- Paste normal mode
-	vim.keymap.set("v", "<D-v>", '"+P') -- Paste visual mode
-	vim.keymap.set("c", "<D-v>", "<C-R>+") -- Paste command mode
-	vim.keymap.set("i", "<D-v>", '<ESC>l"+Pli') -- Paste insert mode
+	vim.keymap.set("n", "<D-v>", '"+P', { desc = "Paste from system clipboard in normal mode" })
+	vim.keymap.set("v", "<D-v>", '"+P', { desc = "Paste from system clipboard in visual mode" })
+	vim.keymap.set("c", "<D-v>", "<C-R>+", { desc = "Paste from system clipboard in command mode" })
+	vim.keymap.set("i", "<D-v>", '<ESC>l"+Pli', { desc = "Paste from system clipboard in insert mode" })
 end
 
 -- Allow clipboard copy paste in neovim
-vim.api.nvim_set_keymap("", "<D-v>", "+p<CR>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("!", "<D-v>", "<C-R>+", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("t", "<D-v>", "<C-R>+", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("v", "<D-v>", "<C-R>+", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("", "<D-v>", "+p<CR>", {
+	noremap = true,
+	silent = true,
+	desc = "Paste from clipboard register and execute in all modes",
+})
+
+vim.api.nvim_set_keymap("!", "<D-v>", "<C-R>+", {
+	noremap = true,
+	silent = true,
+	desc = "Paste from system clipboard in insert and command-line modes",
+})
+
+vim.api.nvim_set_keymap(
+	"t",
+	"<D-v>",
+	'<cmd>lua local clipboard_content = vim.fn.getreg("+"); vim.api.nvim_put({ clipboard_content }, "l", true, true)<CR>',
+	{
+		noremap = true,
+		silent = true,
+		desc = "Paste from system clipboard in terminal mode using Lua function",
+	}
+)
+
+vim.api.nvim_set_keymap("v", "<D-v>", "<C-R>+", {
+	noremap = true,
+	silent = true,
+	desc = "Paste from system clipboard in visual mode",
+})
 
 -- [[ Setting options ]]
 -- See `:help vim.opt`
