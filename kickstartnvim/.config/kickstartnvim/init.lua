98e3ba31a5c3215f99907b6bbe9f7a73a6328783
Jonathan Fok kan <jonathan@fokkan.ca>
2025-05-22 12:43:37 -0400

    feat(nvim): Use nightfox colorscheme with randomization

--- kickstartnvim/.config/kickstartnvim/init.lua
+++ kickstartnvim/.config/kickstartnvim/init.lua
@@ -766,25 +766,29 @@ require('lazy').setup({
     },
   },
 
-  { -- You can easily change to a different colorscheme.
-    -- Change the name of the colorscheme plugin below, and then
-    -- change the command in the config to whatever the name of that colorscheme is.
-    --
-    -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-    'folke/tokyonight.nvim',
-    priority = 1000, -- Make sure to load this before all the other start plugins.
+  {
+    'EdenEast/nightfox.nvim',
+    priority = 1000,
     config = function()
-      ---@diagnostic disable-next-line: missing-fields
-      require('tokyonight').setup {
-        styles = {
-          comments = { italic = false }, -- Disable italics in comments
-        },
+      -- Define the available nightfox colorschemes I want to use
+      local colorschemes = {
+        'terafox',
+        'nightfox',
+        'duskfox',
+        'nordfox',
+        'carbonfox',
       }
 
-      -- Load the colorscheme here.
-      -- Like many other themes, this one has different styles, and you could load
-      -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'tokyonight-night'
+      -- Seed random number generator for better randomness
+      math.randomseed(os.time())
+
+      -- Select a random colorscheme
+      local random_scheme = colorschemes[math.random(#colorschemes)]
+
+      -- Apply the colorscheme
+      vim.cmd.colorscheme(random_scheme)
+
+      vim.notify(string.format('selected colorscheme: %s', random_scheme), vim.log.levels.INFO)
     end,
   },
 
