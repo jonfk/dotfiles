ef4b93d020dfd1e9fccbb9eb37a5f46feeb81ab1
Jonathan Fok kan <jonathan@fokkan.ca>
2025-06-17 11:48:34 -0400

    nvim(telescope): Add keymap for searching all files (hidden/ignored)

--- kickstartnvim/.config/kickstartnvim/init.lua
+++ kickstartnvim/.config/kickstartnvim/init.lua
@@ -316,6 +316,13 @@ require('lazy').setup({
       vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
       vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
       vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
+      vim.keymap.set('n', '<leader>sF', function()
+        builtin.find_files {
+          hidden = true,
+          no_ignore = true,
+          no_ignore_parent = true,
+        }
+      end, { desc = '[S]earch All [F]iles (incl. hid/ignd)' })
 
       -- Slightly advanced example of overriding default behavior and theme
       vim.keymap.set('n', '<leader>/', function()
