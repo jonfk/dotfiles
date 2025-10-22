116f9d8c91250c8b1229332cd71b72604d0cc615
Jonathan Fok kan <jonathan@fokkan.ca>
2025-10-22 10:14:04 -0400

    feat(kickstartnvim): Add nvim-ufo plugin for folding

---
+++ kickstartnvim/.config/kickstartnvim/lua/custom/plugins/folds.lua
@@ -0,0 +1,17 @@
+return {
+  {
+    'kevinhwang91/nvim-ufo',
+    dependencies = { 'kevinhwang91/promise-async' },
+    event = 'VeryLazy', -- You can make it lazy-loaded via VeryLazy, but comment out if anything doesn’t work
+    version = false,
+    init = function()
+      vim.o.foldlevel = 99
+      vim.o.foldlevelstart = 99
+    end,
+    opts = {
+      provider_selector = function(bufnr, filetype, buftype)
+        return { 'treesitter', 'indent' }
+      end,
+    },
+  },
+}
